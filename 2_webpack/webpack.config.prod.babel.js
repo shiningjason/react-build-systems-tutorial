72e5f4edb79c7c8a9c843979821a0351dae6cee4
Chung Jason <shiningjason1989@gmail.com>
2016-05-16 10:43:32 +0800

    Add production build system

---
+++ 2_webpack/webpack.config.prod.babel.js
@@ -0,0 +1,33 @@
+import path from 'path';
+
+import webpack from 'webpack';
+
+export default {
+  entry: './src/main',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
